a31a5af90d2d3db9f6e801feb492ce05eecb50bc
porjesuraj <porjesuraj5@gmail.com>
2020-11-18 15:31:11 +0530

    classwork day 1 'demo on using servlet using annotation '

---
+++ Classwork/Day1_18_Nov/lab1.1/src/pages/HelloServlet.java
@@ -0,0 +1,62 @@
+package pages;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+     // URL ://host:port/Lab_1.1/test
+	// URI : context path (Lab_1.1)
+	// URL Pattern : /test
+
+
+
+@WebServlet(value= {"/test","/test2"},loadOnStartup = 1)
+//WC processes this annotation , at the deployment time and adds the mapping between URL pattern and servlet 
+//WC creates an empty map (Hash Map)
+//key : URL pattern (/test)
+//value : Fully qualified servlet class name 
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		System.out.println("in do-get " + Thread.currentThread());
+	
+		// set response content type for the cln broswwer 
+	
+		resp.setContentType("text/html");
+		
+		// open print writer to send response from servlet --> clnt (text)	
+		
+		try(PrintWriter pw = resp.getWriter())
+		{
+			pw.print("<h1> hello from HelloServlet @" + LocalDateTime.now() + "</h2>"); 
+		}
+	}
+
+	@Override
+	public void destroy() {
+	
+		System.out.println("in destroy  "  + Thread.currentThread());
+	}
+	
+	
+
+	@Override
+	public void init() throws ServletException {
+		
+		System.out.println("in init of " + this.getClass().getName() + " " + Thread.currentThread());
+	}
+
+	
+	
+	
+}
